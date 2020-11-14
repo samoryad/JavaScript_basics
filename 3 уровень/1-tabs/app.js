b30d0ce0a1fe640cfb9be4f27f33f8c73d36a59e
Andrey Samoryadov <samoryad@mail.ru>
2020-11-14 23:27:46 +0300

    3 tasks done

--- 3 уровень/1-tabs/app.js
+++ 3 уровень/1-tabs/app.js
@@ -11,7 +11,11 @@ const texts = {
     2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
     обработчик клика функцию clickHandler.
 */
-
+let textEl = document.querySelector('.text');
+let linkEls = document.querySelectorAll('.nav-link');
+linkEls.forEach(function (link) {
+    link.addEventListener('click', clickHandler);
+})
 
 /**
  * Обработчик клика по .nav-link
@@ -20,7 +24,8 @@ const texts = {
 function clickHandler(event) {
     // здесь вызывайте changeText и changeActiveClass, и передавайте
     // им объект события.
-   
+    changeText(event);
+    changeActiveClass(event);
 }
 
 /**
@@ -29,7 +34,9 @@ function clickHandler(event) {
  * @param {MouseEvent} event 
  */
 function changeActiveClass(event) {
-    
+    let activeEl = document.querySelector('.active');
+    activeEl.classList.remove('active');
+    event.target.classList.add('active');
 }
 
 /**
@@ -39,5 +46,13 @@ function changeActiveClass(event) {
  * @param {MouseEvent} event 
  */
 function changeText(event) {
-    
+    if (event.target.textContent == 'Link 1') {
+        textEl.innerText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.';
+    }
+    if (event.target.textContent == 'Link 2') {
+        textEl.innerText = 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.';
+    }
+    if (event.target.textContent == 'Link 3') {
+        textEl.innerText = 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.';
+    }
 }
