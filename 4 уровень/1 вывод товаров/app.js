12b8c36cbc166f4050f1350fbe2825040386214e
Andrey Samoryadov <samoryad@mail.ru>
2020-11-15 01:17:03 +0300

    homework_07 done

--- 4 уровень/1 вывод товаров/app.js
+++ 4 уровень/1 вывод товаров/app.js
@@ -76,16 +76,22 @@ const products = {
 };
 
 
+let prodsEl = document.querySelector('.products');
+let btnEls = document.querySelectorAll('button');
+btnEls.forEach(function (button) {
+    button.addEventListener('click', clickHandler);
+});
+
 /**
  * Эта функция должна вызываться при клике по кнопкам.
  * @param {MouseEvent} event
  */
 function clickHandler(event) {
     //вам нужно очищать содержимое .products
-    
+    prodsEl.innerHTML = '';
     //в showCategory надо передать строку с типом категории, тип берите
     //из атрибута data-type у кнопки, по которой кликнули.
-    
+    showCategory(event.target.dataset.type);
 }
 
 /**
@@ -96,7 +102,9 @@ function clickHandler(event) {
  * по которой кликнули.
  */
 function showCategory(category) {
-    
+    products[category].forEach(function (productsObj) {
+        prodsEl.insertAdjacentHTML('beforeEnd', getProductMarkup(productsObj));
+    });
 }
 
 /**
@@ -109,5 +117,10 @@ function showCategory(category) {
  * в верху этого файла.
  */
 function getProductMarkup(product) {
-
+    return `<div class="product">
+                <div>${product.name}</div>
+                <img src="${product.imageUrl}" alt="">
+                    <div>${product.price}</div>
+                    <a href="https://example.com/producs/${product.id}">Подробнее</a>
+            </div>`;
 }
